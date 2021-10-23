d3b2e1241135b590b8527add5f88253d9de5343a
Husain Shahid Rao <husain3012@pop-os.localdomain>
2021-10-23 14:04:56 +0530

    config

--- EnSite/settings/prod.py
+++ EnSite/settings/prod.py
@@ -10,6 +10,8 @@ SECRET_KEY = "kjlsdkhdfakojtjsnKKSKALSnUB!GwfaYffsUgfdvd^784tef#()\lmgldm|lmslrf
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','http://enactusjmi.org/','enactus.org','35.244.7.161','127.0.0.1','enactus-300513.el.r.appspot.com','enactus-300513.appspot.com','enactusofficialwebsite-env.eba-xp4xt2nd.ap-south-1.elasticbeanstalk.com']
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 
